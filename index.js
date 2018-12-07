0431a498e223d8b36fb335a336f68806378df940
Moritz Reiter <moritz@anothernode.com>
2018-12-07 10:26:31 +0100

    Wrap chart in grid to facilitate resizing adjustment

--- index.js
+++ index.js
@@ -4,30 +4,41 @@ let blessed = require('blessed');
 let blessedContrib = require('blessed-contrib');
 let screen = blessed.screen();
 
-let netloadBar = blessedContrib.netloadBar({
-    barWidth: 3,
-    barSpacing: 5,
-    xOffset: 0,
-    maxHeight: 3
+let term = require('terminal-kit').terminal;
+
+let grid = new blessedContrib.grid({ rows: 1, cols: 1, screen: screen });
+
+let netloadBar = grid.set(0, 0, 1, 1, blessedContrib.netloadBar, {
+    label: "netload",
+    barWidth: 1,
+    barSpacing: 2,
+    xOffset: 1,
+    maxHeight: 3,
+    showText: false,
 });
 
 screen.append(netloadBar);
 
 let titles = [];
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < term.width; i++) {
     titles.push(' ');
 }
+
 function updateData() {
     let data = [];
     for (let i = 0; i < titles.length; i++) {
-        data.push(Math.round(Math.random() * 10))
+        data.push(Math.round(Math.random() * 9))
     }
-    netloadBar.setData({titles: titles, data: data});
+    netloadBar.setData({ titles: titles, data: data });
     screen.render();
 }
 updateData();
 setInterval(updateData, 1000);
 
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+screen.key(['escape', 'q', 'C-c'], function (ch, key) {
     return process.exit(0);
 });
+
+screen.on('resize', function () {
+    netloadBar.emit('attach');
+})
